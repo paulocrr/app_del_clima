81ebe3056db5aca156e58ce5822186a3f4431b3b
Paulo Rodriguez <paulo_crr@hotmail.com>
2025-02-08 12:20:04 -0500

    chore: Initializacion del proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_del_clima
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_del_clima/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_del_clima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
