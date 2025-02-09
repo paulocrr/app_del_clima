3540392237755815317d65ff7f9076cb7a1ca0a2
Paulo Rodriguez <paulo_crr@hotmail.com>
2025-02-09 13:10:31 -0500

    feat: Se agrego la clase para hacer peticiones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:app_del_clima/configs/configs.dart';
+import 'dart:convert';
+
+import 'package:app_del_clima/core/configs/configs.dart';
+import 'package:app_del_clima/core/networking/network_utility.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -12,10 +15,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final lista = [];
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(Configs.weatherApiKey),
+          child: ElevatedButton(
+            onPressed: () async {
+              final networkUtility = NetworkUtility();
+              final resultados = await Future.wait([
+                networkUtility.peticionesGet(
+                  path: '/weather',
+                  params: {
+                    'q': 'London',
+                    'appid': Configs.weatherApiKey,
+                    'units': 'metric',
+                  },
+                ),
+              ]);
+
+              print(resultados[0]['coord']);
+            },
+            child: Text('test'),
+          ),
         ),
       ),
     );
